00033f8138df0079a747baf85a3b907ed7872069
Grigori Fursin <grigori.fursin@cTuning.org>
2015-03-22 11:24:05 +0100

    fixing building script

--- src/c/ck-make.bat
+++ src/c/ck-make.bat
@@ -10,41 +10,29 @@ rem
 rem Developer(s): Grigori Fursin, 2015
 rem
 
-set CK_SOURCE_FILES=openme.c
+set CK_SOURCE_FILES=cJSON.c openme.c
+set CK_INCLUDE_FILES=cJSON.h openme.h
 set CK_INCLUDE_FILE=openme.h
 
-if [%CK_ENV_SCRIPT_CK%] == [] (
- echo.
- echo Environment variable "CK_ENV_SCRIPT_CK" is empty!
- goto err
-)
+set LIB_NAME=libopenme
 
-if [%CK_ENV_LIB_CJSON%] == [] (
- echo.
- echo Environment variable "CK_ENV_LIB_CJSON" is empty!
- goto err
-)
-
-
-set CK_COMPILER_FLAGS_MISC=%CK_FLAG_PREFIX_INCLUDE%%CK_ENV_LIB_CJSON_INCLUDE% %CK_COMPILER_FLAGS_MISC%
-set CK_LD_FLAGS_MISC=%CK_ENV_LIB_CJSON_LIB%\%CK_ENV_LIB_CJSON_STATIC_NAME%
-
-echo.
-echo Cleaning directory ...
-echo.
-
-call %CK_ENV_SCRIPT_CK%\bin\clean_universal.bat
-
-echo.
-echo Building static library ...
-echo.
+set CK_COMPILER_FLAGS_MISC=%CK_FLAG_PREFIX_INCLUDE%. %CK_COMPILER_FLAGS_MISC%
 
 set CK_TARGET_FILE=%LIB_NAME%%CK_LIB_EXT%
-
 set CK_TARGET_FILE_S=%CK_TARGET_FILE%
 
-call %CK_ENV_SCRIPT_CK%\bin\build_static_lib_c.bat
+set CK_CC_FLAGS=%CK_COMPILER_FLAGS_OBLIGATORY% %CK_COMPILER_FLAGS_MISC% %CK_COMPILER_FLAGS_CC_OPTS% %CK_COMPILER_FLAGS_ARCH% %CK_COMPILER_FLAGS_PAR%
 
+echo Executing %CK_CC% %CK_FLAGS_STATIC_LIB% %CK_FLAGS_CREATE_OBJ% %CK_CC_FLAGS% %CK_SOURCE_FILES% %CK_LD_FLAGS_MISC% %CK_LD_FLAGS_EXTRA%
+%CK_CC% %CK_FLAGS_STATIC_LIB% %CK_FLAGS_CREATE_OBJ% %CK_CC_FLAGS% %CK_SOURCE_FILES% %CK_LD_FLAGS_MISC% %CK_LD_FLAGS_EXTRA%
+if %errorlevel% neq 0 (
+ echo.
+ echo Building failed!
+ goto err
+)
+
+echo Executing %CK_LB% %CK_LB_OUTPUT%%CK_TARGET_FILE% *%CK_OBJ_EXT%
+%CK_LB% %CK_LB_OUTPUT%%CK_TARGET_FILE% *%CK_OBJ_EXT%
 if %errorlevel% neq 0 (
  echo.
  echo Building failed!
@@ -55,12 +43,15 @@ echo.
 echo Building dynamic library ...
 echo.
 
+set CK_SOURCE_FILES=cJSON.c
 set CK_TARGET_FILE=%LIB_NAME%%CK_DLL_EXT%
 
 set CK_TARGET_FILE_D=%CK_TARGET_FILE%
 
-call %CK_ENV_SCRIPT_CK%\bin\build_dynamic_lib_c.bat
+set CK_CC_FLAGS=%CK_COMPILER_FLAGS_OBLIGATORY% %CK_COMPILER_FLAGS_MISC% %CK_COMPILER_FLAGS_CC_OPTS% %CK_COMPILER_FLAGS_ARCH% %CK_COMPILER_FLAGS_PAR%
 
+echo Executing %CK_CC% %CK_FLAGS_DLL% %CK_CC_FLAGS% %CK_SOURCE_FILES% %CK_FLAGS_OUTPUT%%CK_TARGET_FILE% %CK_FLAGS_DLL_EXTRA% %CK_LD_FLAGS_MISC% %CK_LD_FLAGS_EXTRA%
+%CK_CC% %CK_FLAGS_DLL% %CK_CC_FLAGS% %CK_SOURCE_FILES% %CK_FLAGS_OUTPUT%%CK_TARGET_FILE% %CK_FLAGS_DLL_EXTRA% %CK_LD_FLAGS_MISC% %CK_LD_FLAGS_EXTRA%
 if %errorlevel% neq 0 (
  echo.
  echo Building failed!
@@ -76,7 +67,7 @@ copy /B %CK_TARGET_FILE_S% ..\..\lib
 copy /B %CK_TARGET_FILE_D% ..\..\lib
 
 mkdir ..\..\include
-copy /B %CK_INCLUDE_FILE% ..\..\include
+copy /B %CK_INCLUDE_FILES% ..\..\include
 
 
 exit /b 0
